d12ea0ede0570406cf20046676b2ab7c2a0f045d
Jash Maniar <101001262+JashManiar@users.noreply.github.com>
2022-08-30 21:49:59 +0000

    Update main.js

--- main.js
+++ main.js
@@ -11,10 +11,10 @@ function start()
  recognition.onresult=function(event)
  
  {console.log(event);
-var Content=event.results[0][0].transcript;
-document.getElementById("textbox").innerHTML=Content;
+var Content = event.results[0][0].transcript;
+document.getElementById("textbox").innerHTML= Content;
 console.log(Content);
-if(Content=="selfie")
+if(Content =="selfie")
 {console.log("taking selfie---");
 speak();}
 }
